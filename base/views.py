422cd08cd015c1e9050d05b7400386f9103fb429
Dada Oluwashina <dada.oluwashina@longbridgetech.com>
2022-09-05 01:19:01 +0100

    used generic class based view to build crud api

--- base/views.py
+++ base/views.py
@@ -2,13 +2,16 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializer import TeacherSerializer 
-from . import models 
+from rest_framework import generics
+from . import models
 
 
 
 
-class TeacherList(APIView):
-    def get(self, request):
-        teachers=models.Teacher.objects.all()
-        serializer = TeacherSerializer(teachers, many=True)
-        return Response(serializer.data)
+class TeacherList(generics.ListCreateAPIView):
+    queryset=models.Teacher.objects.all()
+    serializer_class=TeacherSerializer
+
+class Teacher_update_destroy_detail(generics.RetrieveUpdateDestroyAPIView):
+    queryset=models.Teacher.objects.all()
+    serializer_class=TeacherSerializer    
