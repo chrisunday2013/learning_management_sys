45d279d764806d4e2e3db0c0145036c497c81b47
Dada Oluwashina <dada.oluwashina@longbridgetech.com>
2022-09-05 01:44:30 +0100

    basic authentication to api with django permission_classes

--- base/views.py
+++ base/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializer import TeacherSerializer 
+from rest_framework import permissions
 from rest_framework import generics
 from . import models
 
@@ -11,7 +12,10 @@ from . import models
 class TeacherList(generics.ListCreateAPIView):
     queryset=models.Teacher.objects.all()
     serializer_class=TeacherSerializer
+    permission_classes=[permissions.IsAuthenticated]
+
 
 class Teacher_update_destroy_detail(generics.RetrieveUpdateDestroyAPIView):
     queryset=models.Teacher.objects.all()
-    serializer_class=TeacherSerializer    
+    serializer_class=TeacherSerializer 
+    permission_classes=[permissions.IsAuthenticated]   
